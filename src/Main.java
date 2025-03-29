90d71b046594f8f4014758432dfc6e097e7b129d
r.solikhov <r.solikhov@g.nsu.ru>
2025-03-29 05:24:37 +0300

    Added: controller, view, listeners

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import Controller.Controller;
+import UI.View;
+
+import javax.swing.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+        View v = new View();
+        Controller controller = new Controller(v);
+
+    }
+
 }
